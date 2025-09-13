fd007bbda7514a95a22351a10f543d06dbd8795b
김은영 <kimeunyoung-0425@naver.com>
2025-09-14 00:53:25 +0900

    실습_React 컴포넌트

--- onebite-react/React/react-pjt/src/App.jsx
+++ onebite-react/React/react-pjt/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Header from "./components/Header"; // (ESM) vite로 만든 리액트 앱에서 확장자 생략 가능
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
+// 컴포넌트
+// - HTML 태그를 반환하는 함수 (함수 선언식, 화살표 함수)
+// - 함수명 첫글자는 대문자!
+// - 리액트 컴포넌트는 App 컴포넌트를 최상위 조상으로 갖는 계층 구조를 가짐 => Root 컴포넌트
 function App() {
   return (
     <>
-      <h1>React</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
