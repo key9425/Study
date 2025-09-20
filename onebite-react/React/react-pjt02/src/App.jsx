b09e204a6d0fe1f010c233bf3e2f8adf7749a493
김은영 <kimeunyoung-0425@naver.com>
2025-09-20 19:10:08 +0900

    실습_실습 pjt02 준비

---
+++ onebite-react/React/react-pjt02/src/App.jsx
@@ -0,0 +1,68 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+
+// 임시 데이터
+// - 컴포넌트가 리렌더링 될 때마다 다시 생성될 필요 없음
+// - 상수이기 때문에 값 변경 불가
+// -> 컴포넌트 외부 선언
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "Recat 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래연습하기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+
+    // state 값은 반드시 상태변화함수를 호출해서 수정
+    // -> 변경된 state 값을 리액트가 감지하여 컴포넌트를 정상적으로 리렌더링 할 수 있음
+    setTodos([newTodo, ...todos]);
+  };
+
+  // 배열이나 객체 형태의 State를 업데이트할 때 기존 State를 직접 수정하지 않고 새 객체/배열을 만들어 사용하는 이유
+  // - React의 State 변경 감지 및 리렌더링 동작 (React는 State 객체/배열의 '참조'가 변경될 때 리렌더링을 결정 -> 새 객체/배열을 만들면 참조가 달라진다)
+  const onUpdate = (targetId) => {
+    setTodos(todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo)));
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default App;
---
+++ onebite-react/React/react-pjt02/src/components/List.jsx
@@ -0,0 +1,36 @@
+import "./List.css";
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()));
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 📝</h4>
+      <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        {/* 배열에 담긴 데이터를 리스트 형태로 렌더링 */}
+        {/* 리스트 형태로 렌더링된 컴포넌트(요소)는 구분하기 위해 반드시 key라는 prop을 고유한 값으로 전달 */}
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
