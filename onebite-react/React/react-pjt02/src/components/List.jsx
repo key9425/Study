820edc29949d235a499fb81a423389669db94196
김은영 <kimeunyoung-0425@naver.com>
2025-09-21 22:17:33 +0900

    실습_React useMemo

--- onebite-react/React/react-pjt02/src/components/List.jsx
+++ onebite-react/React/react-pjt02/src/components/List.jsx
@@ -1,5 +1,5 @@
 import "./List.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import TodoItem from "./TodoItem";
 
 const List = ({ todos, onUpdate, onDelete }) => {
@@ -18,9 +18,33 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  // useMemo
+  // - "메모이제이션" 기법을 기반으로 불필요한 연산을 방지하여 최적화 하는 리액트 훅
+  // - 두번째 인수로 전달한 deps를 기준으로 첫번째 인수로 전달한 콜백함수를 메모이제이션
+  // - useMemo 호출의 결과값 = 콜백함수의 반환값
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    // - 메모이제이션하고 싶은 연산
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // - 의존성배열(deps) : 배열에 포함된 값이 변경되었을 때만 콜백함수를 다시 실행
+  // - 빈 deps인 경우 컴포넌트가 최초로 렌더링될 때만 실행
+
   return (
     <div className="List">
       <h4>Todo List 📝</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
         {/* 배열에 담긴 데이터를 리스트 형태로 렌더링 */}
