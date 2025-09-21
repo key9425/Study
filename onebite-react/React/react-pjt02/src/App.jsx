25f62b5d82e465cf8c54a6d120796c849e1d7c98
김은영 <kimeunyoung-0425@naver.com>
2025-09-22 02:36:23 +0900

    실습_React 최적화 정리

--- onebite-react/React/react-pjt02/src/App.jsx
+++ onebite-react/React/react-pjt02/src/App.jsx
@@ -5,6 +5,15 @@ import Header from "./components/Header";
 import List from "./components/List";
 // import ExamReducer from "./components/ExamReducer";
 
+// React App 내부의 최적화 방법
+// - 컴포넌트 내부의 불필요한 연산 방지 -> useMemo
+// - 컴포넌트 내부의 불필요한 함수 재생성 방지 -> useCallback
+// - 컴포넌트의 불필요한 리렌더링 방지 -> React.memo
+
+// 최적화 기준
+// - 프로젝트를 완성한 상태에서 최적화 (1.기능 -> 2.최적화)
+// - 최적화가 필요할 것 같은 연산, 함수, 컴포넌트에만 적용
+
 // 임시 데이터
 // - 컴포넌트가 리렌더링 될 때마다 다시 생성될 필요 없음
 // - 상수이기 때문에 값 변경 불가
